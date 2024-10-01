7508506aa5defe45aa2e759b72d59b7b5a072b89
Justin Conabree <jconabree@absolunet.com>
2024-10-01 18:33:20 -0400

    Using freeze to fix gap issue with wled, commented code for heartbeat effect

--- src/api/src/models/initiative.ts
+++ src/api/src/models/initiative.ts
@@ -5,8 +5,9 @@ import { getAllAreas } from "./areas";
 export const publishCharacterStates = async (characters: CharacterArgumentData[]) => {
     console.log('updating player states', characters);
 
-    const segmentData = characters.reduce<{ id: number, on: Boolean, col: number[][]}[]>((segments, character) => {
+    const segmentData = characters.reduce<{ id: number, on: Boolean, frz: Boolean, col: number[][]}[]>((segments, character) => {
         let rgb = [255, 255, 255];
+        let effectData : { fx: number, sx?: number, ix?: number } = { fx: 0 };
         if (typeof character.currentHealth === 'number' && typeof character.maxHealth  === 'number') {
             const percentage = (character.currentHealth / character.maxHealth) * 100;
 
@@ -18,18 +19,29 @@ export const publishCharacterStates = async (characters: CharacterArgumentData[]
             const red = 255 - ((percentage / 100) * 255)
             
             rgb = [red, green, 5];
+
+            // if (percentage < 20) {
+            //     effectData = {
+            //         fx: 100,
+            //         sx: percentage * 5,
+            //         ix: percentage < 5 ? 225 : 165
+            //     }
+            // }
         }
 
         segments.push({
             id: character.area.segment,
             on: true,
-            col: [rgb]
+            frz: false,
+            col: [rgb, [0, 0, 0], [0, 0, 0]],
+            ...effectData
         });
 
         if (typeof character.area.segmentPre === 'number') {
             segments.push({
                 id: character.area.segmentPre,
                 on: character.isCurrent,
+                frz: !character.isCurrent,
                 col: [[255, 255, 255]]
             })
         }
@@ -38,6 +50,7 @@ export const publishCharacterStates = async (characters: CharacterArgumentData[]
             segments.push({
                 id: character.area.segmentPost,
                 on: character.isCurrent,
+                frz: !character.isCurrent,
                 col: [[255, 255, 255]]
             })
         }
@@ -69,16 +82,18 @@ export const publishCharacterStates = async (characters: CharacterArgumentData[]
 export const stopInitiative = async (keepHealth?: boolean) => {
     const allAreas = getAllAreas();
 
-    const segmentData = allAreas.reduce<{ id: number, on: Boolean}[]>((segments, area) => {
+    const segmentData = allAreas.reduce<{ id: number, on: Boolean, frz: Boolean}[]>((segments, area) => {
         segments.push({
             id: area.segment,
             on: Boolean(keepHealth),
+            frz: !Boolean(keepHealth)
         });
 
         if (typeof area.segmentPre === 'number') {
             segments.push({
                 id: area.segment,
                 on: false,
+                frz: true
             });
         }
 
@@ -86,6 +101,7 @@ export const stopInitiative = async (keepHealth?: boolean) => {
             segments.push({
                 id: area.segmentPost,
                 on: false,
+                frz: true,
             });
         }
 
